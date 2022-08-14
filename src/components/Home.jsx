9ae2e469cb5e08b3962345a791d8bbdc434acb32
Shane <shaneboo152@gmail.com>
2022-08-14 00:11:03 -0700

    fix a protected route issue

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -80,7 +80,6 @@ function Home() {
   }, [serverId]);
 
   console.log("this is serverId from home", channelId);
- 
 
   const addServer = async () => {
     const name = prompt("Enter a name for the new server");
@@ -93,7 +92,7 @@ function Home() {
     // add default channell
     if (docRef) {
       await addDoc(collection(db, "servers", docRef.id, "channels"), {
-        channelName: "general", 
+        channelName: "general",
         timeStamp: firebase.firestore.FieldValue.serverTimestamp(),
       });
     }
@@ -119,86 +118,97 @@ function Home() {
 
   return (
     <>
-      {!user && <Navigate replace to="/" />}
-      <div className="flex h-screen">
-        <div className="flex flex-col bg-discord_serverContainer overflow-scroll scrollbar-hide  items-center  space-y-3 p-3 min-w-max">
-          <div className="server-default hover:bg-discord_purple flex items-center justify-center">
-            <img src="../topicon.png" alt="" className="h-10 p-2" />
-          </div>
-          <hr className="border-gray-700 border w-8 mx-auto" />
+      {user ? (
+        <div className="flex h-screen">
+          <div className="flex flex-col bg-discord_serverContainer overflow-scroll scrollbar-hide  items-center  space-y-3 p-3 min-w-max">
+            <div className="server-default hover:bg-discord_purple flex items-center justify-center">
+              <img src="../topicon.png" alt="" className="h-10 p-2" />
+            </div>
+            <hr className="border-gray-700 border w-8 mx-auto" />
 
-          {servers?.map((server) => (
-            <ServerIcon key={server.id} id={server.id} server={server.data()} />
-          ))}
+            {servers?.map((server) => (
+              <ServerIcon
+                key={server.id}
+                id={server.id}
+                server={server.data()}
+              />
+            ))}
 
-          <div className="server-default hover:bg-discord_green  p-3 group  ">
-            <PlusIcon
-              onClick={addServer}
-              className="text-discord_green w-7 h-7 group-hover:text-white"
-            />
-          </div>
-        </div>
-
-        <div className="bg-discord_channelsBg flex flex-col min-w-max">
-          <h2
-            className="text-white font-bold text-sm flex items-center justify-between
-          border-b border-gray-800 p-4 cursor-pointer
-          hover:bg-discord_serverNameHoverBg"
-          >
-            Official Server
-            <ChevronDownIcon className="h-5 ml-2" />
-          </h2>
-
-          <div className="text-discord_channel flex-grow overflow-y-scroll scrollbar-hide">
-            <div className="flex items-center p-2 mb-2">
-              <ChevronDownIcon className="h-3 mr-2" />
-              <h4 className="font-semibold">channels</h4>
+            <div className="server-default hover:bg-discord_green  p-3 group  ">
               <PlusIcon
-                className="h-6 ml-auto cursor-pointer hover:text-white"
-                onClick={handleAddChannel}
+                onClick={addServer}
+                className="text-discord_green w-7 h-7 group-hover:text-white"
               />
             </div>
+          </div>
 
-            <div className="flex flex-col space-y-2 px-2 mb-4">
-              {channels?.map((doc) => {
-                return (
-                  <Channel
-                    id={doc.id}
-                    key={doc.id}
-                    channelName={doc.data().channelName}
-                  />
-                );
-              })}
+          <div className="bg-discord_channelsBg flex flex-col min-w-max">
+            <h2
+              className="text-white font-bold text-sm flex items-center justify-between
+          border-b border-gray-800 p-4 cursor-pointer
+          hover:bg-discord_serverNameHoverBg"
+            >
+              Official Server
+              <ChevronDownIcon className="h-5 ml-2" />
+            </h2>
+
+            <div className="text-discord_channel flex-grow overflow-y-scroll scrollbar-hide">
+              <div className="flex items-center p-2 mb-2">
+                <ChevronDownIcon className="h-3 mr-2" />
+                <h4 className="font-semibold">channels</h4>
+                <PlusIcon
+                  className="h-6 ml-auto cursor-pointer hover:text-white"
+                  onClick={handleAddChannel}
+                />
+              </div>
+
+              <div className="flex flex-col space-y-2 px-2 mb-4">
+                {channels?.map((doc) => {
+                  return (
+                    <Channel
+                      id={doc.id}
+                      key={doc.id}
+                      channelName={doc.data().channelName}
+                    />
+                  );
+                })}
+              </div>
+            </div>
+
+            <div className="flex bg-[#292b2f] p-2 justify-between items-center space-x-8  cursor-pointer pr-4">
+              <div className="flex items-center space-x-1 ">
+                <img
+                  src={user?.photoURL}
+                  alt=""
+                  className="h-10 rounded-full"
+                />
+
+                <h4 className="text-white text-xs font-medium">
+                  {user?.displayName}
+                  <span className="text-[#b9bbbe] block">
+                    #{user?.uid.substring(0, 4)}
+                  </span>
+                </h4>
+              </div>
+
+              <div className="hover:bg-[#3a3c43] p-2 rounded-md mr-5">
+                <h1
+                  onClick={() => auth.signOut()}
+                  className="font-bold text-white mx-auto "
+                >
+                  Logout
+                </h1>
+              </div>
             </div>
           </div>
 
-          <div className="flex bg-[#292b2f] p-2 justify-between items-center space-x-8  cursor-pointer pr-4">
-            <div className="flex items-center space-x-1 ">
-              <img src={user?.photoURL} alt="" className="h-10 rounded-full" />
-
-              <h4 className="text-white text-xs font-medium">
-                {user?.displayName}
-                <span className="text-[#b9bbbe] block">
-                  #{user?.uid.substring(0, 4)}
-                </span>
-              </h4>
-            </div>
-
-            <div className="hover:bg-[#3a3c43] p-2 rounded-md mr-5">
-              <h1
-                onClick={() => auth.signOut()}
-                className="font-bold text-white mx-auto "
-              >
-                Logout
-              </h1>
-            </div>
+          <div className="bg-[#36393f] flex-grow">
+            <Chat />
           </div>
         </div>
-
-        <div className="bg-[#36393f] flex-grow">
-          <Chat />
-        </div>
-      </div>
+      ) : (
+        navigate("/")
+      )}
     </>
   );
 }
